04ae9cb49ee976b34442645a8bc77d70d8f812d3
Raczan <51896000+Raczan@users.noreply.github.com>
2024-01-17 01:42:10 -0600

    Added functionality to edit/ad user

--- src/app/users/columns.tsx
+++ src/app/users/columns.tsx
@@ -1,92 +1,139 @@
 "use client";
-
+import * as React from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-// This type is used to define the shape of our data.
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { table } from "@/app/users/data-table";
+import { AddUser } from "@/components/add-user";
+
+async function deleteUser(id: string) {
+  const deleteEndPoint = `https://n913tmwy61.execute-api.us-east-2.amazonaws.com/items/${id}`;
+  const response = await fetch(deleteEndPoint, {
+    method: "DELETE",
+  });
+  const resDeletion = await response.json();
+  console.log(resDeletion);
+}
 
 export type User = {
-	nombre: string;
-	codigo_postal: string;
-	fecha_nacimiento: Date;
-	provincia: string;
-	apellido2: string;
-	email: string;
-	apellido1: string;
-	numero_via: number;
-	direccion_completa: string;
-	direccion: string;
-	ssn: string;
-	tarjeta_fecha: Date;
-	nombre_completo: string;
-	iban: string;
-	municipio: string;
-	contrasenya: string;
-	pasaporte: string;
-	telefono: string;
-	nombre_usuario: string;
-	tarjeta_cvc: number;
-	bic: string;
-	id: string;
-	tarjeta: string;
+  nombre: string;
+  codigo_postal: string;
+  fecha_nacimiento: Date;
+  provincia: string;
+  apellido2: string;
+  email: string;
+  apellido1: string;
+  numero_via: number;
+  direccion_completa: string;
+  direccion: string;
+  ssn: string;
+  tarjeta_fecha: Date;
+  nombre_completo: string;
+  iban: string;
+  municipio: string;
+  contrasenya: string;
+  pasaporte: string;
+  telefono: string;
+  nombre_usuario: string;
+  tarjeta_cvc: number;
+  bic: string;
+  id: string;
+  tarjeta: string;
 };
 
 export const columns: ColumnDef<User>[] = [
-	{
-		accessorKey: "id",
-		header: "ID",
-	},
-	{
-		accessorKey: "nombre_completo",
-		header: "Nombre Completo",
-	},
-	{
-		accessorKey: "nombre_usuario",
-		header: "Usuario",
-	},
-	{
-		id: "actions",
-		cell: ({ row }) => {
-			const user = row.original;
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "nombre_completo",
+    header: "Nombre Completo",
+  },
+  {
+    accessorKey: "nombre_usuario",
+    header: "Usuario",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const meta = table.options.meta;
+      const user = row.original;
+      const [open, setOpen] = React.useState(false);
+      return (
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Abrir menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => {
+                  setOpen(true);
+                }}
+              >
+                Editar
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
 
-			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-							<span className="sr-only">Abrir menu</span>
-							<MoreHorizontal className="h-4 w-4" />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>Acciones</DropdownMenuLabel>
-						<DropdownMenuItem
-							onClick={() => console.log("Viendo..." + user.id)}
-						>
-							Detalles
-						</DropdownMenuItem>
-						<DropdownMenuItem
-							onClick={() => console.log("Editanto..." + user.id)}
-						>
-							Editar
-						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem
-							onClick={() => console.log("Eliminando... " + user.id)}
-						>
-							Eliminar
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			);
-		},
-	},
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem>Eliminar</DropdownMenuItem>
+              </AlertDialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Eliminar usuario</AlertDialogTitle>
+              <AlertDialogDescription>
+                ¿Estas seguro de que quieres eliminar al usuario{" "}
+                {user?.nombre_usuario}?, esta accion no puede ser revertida.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  meta.removeUser(row.index);
+                  deleteUser(user.id);
+                }}
+              >
+                Eliminar
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+          <AddUser
+            hideBtn
+            open={open}
+            setOpen={setOpen}
+            type="Editar"
+            user={user}
+          />
+        </AlertDialog>
+      );
+    },
+  },
 ];
--- src/components/add-user.tsx
+++ src/components/add-user.tsx
@@ -1,264 +1,213 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { table } from "@/app/users/data-table";
 
-export function AddUser() {
-	const formSchema = z.object({
-		nombre: z.string().trim().min(1, { message: "El nombre es obligatorio" }),
-		codigo_postal: z.string(),
-		fecha_nacimiento: z.string(),
-		provincia: z.string(),
-		apellido2: z.string(),
-		email: z.string().email("El correo no es valido"),
-		apellido1: z
-			.string()
-			.trim()
-			.min(1, { message: "El primer apellido es obligatorio" }),
-		numero_via: z.coerce.number(),
-		direccion_completa: z.string(),
-		direccion: z.string(),
-		ssn: z.string(),
-		tarjeta_fecha: z.string(),
-		iban: z.string(),
-		municipio: z.string(),
-		contrasenya: z
-			.string()
-			.min(5, {
-				message: "La contraseña debe tener un minimo de 5 caracteres",
-			})
-			.max(30, {
-				message: "La contraseña no puede exceder 20 caracteres",
-			}),
-		pasaporte: z.string(),
-		telefono: z.string(),
-		nombre_usuario: z
-			.string()
-			.trim()
-			.min(1, { message: "Nombre de usuario es obligatorio" }),
-		tarjeta_cvc: z.number({ invalid_type_error: "Ingrese un cvc valida" }),
-		bic: z.string(),
-		id: z.string().trim().min(1, { message: "ID es obligatorio" }),
-		tarjeta: z.string(),
-	});
+export function AddUser({ hideBtn, open, setOpen, type, user }) {
+  const formSchema = z.object({
+    nombre: z.string().trim().min(1, { message: "El nombre es obligatorio" }),
+    codigo_postal: z.string(),
+    fecha_nacimiento: z.string(),
+    provincia: z.string(),
+    apellido2: z.string(),
+    email: z.string(),
+    apellido1: z
+      .string()
+      .trim()
+      .min(1, { message: "El primer apellido es obligatorio" }),
+    numero_via: z.string(),
+    direccion_completa: z.string(),
+    direccion: z.string(),
+    ssn: z.string(),
+    tarjeta_fecha: z.string(),
+    nombre_completo: z.string(),
+    iban: z.string(),
+    municipio: z.string(),
+    contrasenya: z.string(),
+    pasaporte: z.string(),
+    telefono: z.string(),
+    nombre_usuario: z
+      .string()
+      .trim()
+      .min(1, { message: "Nombre de usuario es obligatorio" }),
+    tarjeta_cvc: z.string(),
+    bic: z.string(),
+    id: z
+      .string()
+      .regex(
+        new RegExp(/d{8}[A-Z]/),
+        "Primeros 8 caracteres son dígitos, y el último es una letra mayúscula"
+      )
+      .min(1, { message: "ID es obligatorio" }),
+    tarjeta: z.string(),
+  });
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			nombre: "",
-			codigo_postal: "",
-			fecha_nacimiento: new Date().toDateString(),
-			provincia: "",
-			apellido2: "",
-			email: "",
-			apellido1: "",
-			numero_via: 789,
-			direccion_completa: "",
-			direccion: "",
-			ssn: "",
-			tarjeta_fecha: new Date().toDateString(),
-			iban: "",
-			municipio: "",
-			contrasenya: Math.random().toString(36).substring(2, 11),
-			pasaporte: "",
-			telefono: "",
-			nombre_usuario: "",
-			tarjeta_cvc: 130,
-			bic: "",
-			id: Math.random().toString(36).substring(3, 12),
-			tarjeta: "",
-		},
-	});
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nombre: user ? user.nombre : "",
+      codigo_postal: user ? user.codigo_postal : "",
+      fecha_nacimiento: "",
+      provincia: "",
+      apellido2: user ? user.apellido2 : "",
+      email: "",
+      apellido1: user ? user.apellido1 : "",
+      numero_via: "",
+      direccion_completa: "",
+      direccion: "",
+      ssn: "",
+      tarjeta_fecha: "",
+      nombre_completo: "",
+      iban: "",
+      municipio: "",
+      contrasenya: "",
+      pasaporte: "",
+      telefono: "",
+      nombre_usuario: user ? user.nombre_usuario : "",
+      tarjeta_cvc: "",
+      bic: "",
+      id: user ? user.id : "",
+      tarjeta: "",
+    },
+  });
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		console.log(values);
-	}
+  async function putUser(payload: object) {
+    const putEndPoint =
+      "https://n913tmwy61.execute-api.us-east-2.amazonaws.com/items";
+    const response = await fetch(putEndPoint, {
+      method: "PUT",
+      body: JSON.stringify(payload),
+    });
+    const resPut = await response.json();
+    setOpen(false);
+    console.log(resPut);
+  }
 
-	return (
-		<Dialog
-			onOpenChange={() => {
-				form.reset();
-			}}
-		>
-			<DialogTrigger asChild>
-				<Button variant="default">Agregar</Button>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Agregar Usuario</DialogTitle>
-					<DialogDescription>
-						Llena los siguientes campos para crear un usuario. Haga clic en
-						guardar cuando haya terminado.
-					</DialogDescription>
-				</DialogHeader>
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    values.nombre_completo =
+      values.nombre + " " + values.apellido1 + " " + values.apellido2;
+    putUser(values);
+    const meta = table.options.meta;
+    meta.adUser(values);
+  }
 
-				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-						<section>
-							<Label>Nombre completo</Label>
-							<div className="flex flex-col gap-2 lg:flex-row mt-2">
-								<FormField
-									control={form.control}
-									name="nombre"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Nombre" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="apellido1"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Primer apellido" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="apellido2"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Segundo apellido" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-							</div>
-						</section>
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Correo electronico</FormLabel>
-									<FormControl>
-										<Input placeholder="Email" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="telefono"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Telefono</FormLabel>
-									<FormControl>
-										<Input placeholder="Telefono" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<section className="flex flex-col gap-2">
-							<Label>Direccion completa</Label>
-							<FormField
-								control={form.control}
-								name="direccion"
-								render={({ field }) => (
-									<FormItem>
-										<FormControl>
-											<Input placeholder="Direccion" {...field} />
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<div className="flex flex-col gap-2 lg:flex-row">
-								<FormField
-									control={form.control}
-									name="numero_via"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input
-													placeholder="No. Via"
-													{...field}
-													inputMode="numeric"
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="codigo_postal"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Codigo postal" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-							</div>
-							<div className="flex flex-col gap-2 lg:flex-row">
-								<FormField
-									control={form.control}
-									name="municipio"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Municipio" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="provincia"
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<Input placeholder="Provincia" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-							</div>
-						</section>
-						<DialogFooter>
-							<Button type="submit">Guardar</Button>
-						</DialogFooter>
-					</form>
-				</Form>
-			</DialogContent>
-		</Dialog>
-	);
+  useEffect(() => {
+    if (!open) {
+      form.reset();
+    }
+  }, [open]);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild className={hideBtn ? "hidden" : ""}>
+        <Button variant="default">Agregar</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{type} Usuario</DialogTitle>
+          <DialogDescription>
+            Llena los siguientes campos para {type} un usuario. Haga clic en
+            guardar cuando haya terminado.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <section>
+              <Label>Nombre completo</Label>
+              <FormField
+                control={form.control}
+                name="nombre"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Nombre" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex flex-col gap-2 lg:flex-row mt-2">
+                <FormField
+                  control={form.control}
+                  name="apellido1"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="Primer apellido" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="apellido2"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="Segundo apellido" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </section>
+            <FormField
+              control={form.control}
+              name="nombre_usuario"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre usuario</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nombre usuario" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="id"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>ID</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Id unico" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Guardar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
 }
